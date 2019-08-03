feafefa3bfe855f1c08daabb1b90889e6b5241bd
rosalia200 <wasongarosa@gmail.com>
2019-08-03 08:38:56 -0700

    production

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 from flask import Flask,render_template,request,redirect,url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 from config import Development,Testing
+from config import Production
 #instatiating class Flask
 app = Flask(__name__)
-app.config.from_object(Development)
+#app.config.from_object(Development)
 # app.config.from_object(Testing)#comment if you want to develop
-#app.config.from_object(Production)
+app.config.from_object(Production)
 
 db= SQLAlchemy(app)
 from models.Employees import  EmployeeModel
